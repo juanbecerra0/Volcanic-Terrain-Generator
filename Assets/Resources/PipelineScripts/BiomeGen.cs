30935f9b228fd7a5e86684f54fd34b1c107f7c6f
juanbecerra0 <47235983+juanbecerra0@users.noreply.github.com>
2020-04-18 17:02:35 -0600

    Implemented debugging measures for biome

--- Assets/Resources/PipelineScripts/BiomeGen.cs
+++ Assets/Resources/PipelineScripts/BiomeGen.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 public class BiomeGen : MonoBehaviour
 {
     private static int BiomeDimensions;
+    private int SeedSpacing;
     private uint Water, Sand, Grass, Mountain, Snow;
 
-    public void Init(int biomeDimensions, uint waterSymbol, uint sandSymbol, uint grassSymbol, uint mountainSymbol, uint snowSymbol)
+    public void Init(int biomeDimensions, int seedSpacing, uint waterSymbol, uint sandSymbol, uint grassSymbol, uint mountainSymbol, uint snowSymbol)
     {
         BiomeDimensions = biomeDimensions;
+        SeedSpacing = seedSpacing;
         Water = waterSymbol;
         Sand = sandSymbol;
         Grass = grassSymbol;
@@ -20,11 +23,53 @@ public class BiomeGen : MonoBehaviour
 
     private static uint[,] Biome;
 
+    public void WriteAsTextFile(uint[,] Biome, String filepath)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < BiomeDimensions; i++)
+        {
+            for (int j = 0; j < BiomeDimensions; j++)
+            {
+                sb.Append(Biome[i, j] + " ");
+            }
+            sb.Append("\n");
+        }
+
+        System.IO.File.WriteAllText(filepath, sb.ToString());
+    }
+
+    public void WriteAsTexture(uint[,] Biome, String filepath)
+    {
+        Texture2D image = new Texture2D(BiomeDimensions, BiomeDimensions);
+
+        for (int i = 0; i < BiomeDimensions; i++)
+        {
+            for (int j = 0; j < BiomeDimensions; j++)
+            {
+                if (Biome[i, j] == 3)
+                    image.SetPixel(i, j, Color.green);
+                else if (Biome[i, j] == 4)
+                    image.SetPixel(i, j, Color.grey);
+                else if (Biome[i, j] == 5)
+                    image.SetPixel(i, j, Color.white);
+                else
+                    image.SetPixel(i, j, Color.red);
+            }
+        }
+
+        byte[] _bytes = image.EncodeToPNG();
+        System.IO.File.WriteAllBytes(filepath, _bytes);
+        Debug.Log(_bytes.Length / 1024 + "Kb was saved as: " + filepath);
+
+        //GUI.DrawTexture(new Rect(0, 0, BiomeDimensions, BiomeDimensions), image);
+    }
+
     private class SeedAgent
     {
-        private uint BiomeType;
-        private int X;
-        private int Y;
+        private readonly uint BiomeType;
+        private readonly int X;
+        private readonly int Y;
 
         private SeedAgent(uint BiomeType, int X, int Y)
         {
@@ -37,7 +82,7 @@ public class BiomeGen : MonoBehaviour
 
         public static SeedAgent Create(uint BiomeType, int X, int Y)
         {
-            if (X >= 0 && X < BiomeDimensions && Y >= 0 && Y < BiomeDimensions)
+            if (X >= 0 && X < BiomeDimensions && Y >= 0 && Y < BiomeDimensions && Biome[X, Y] == 0)
                 return new SeedAgent(BiomeType, X, Y);
             else
                 return null;
@@ -53,10 +98,12 @@ public class BiomeGen : MonoBehaviour
         Biome = new uint[BiomeDimensions, BiomeDimensions];
 
         Queue<SeedAgent> AgentQueue = GetSeedAgentQueue();
-        
+        int writes = 0;
+
         // BFS the AgentQueue, marking adjacent coordinates of Biome
-        while(AgentQueue.Count > 0)
+        while (AgentQueue.Count > 0)
         {
+            writes++;
             SeedAgent agent = AgentQueue.Dequeue();
 
             int cycle = UnityEngine.Random.Range(1, 8);
@@ -118,6 +165,7 @@ public class BiomeGen : MonoBehaviour
             }
         }
 
+        Debug.Log(writes);
         return Biome;
     }
 
@@ -140,38 +188,38 @@ public class BiomeGen : MonoBehaviour
         AgentQueue.Enqueue(snowAgent);
 
         // Enqueue several mountain agents
-        SeedAgent mountainAgent1 = SeedAgent.Create(Mountain, snowAgent.GetX() + 2, snowAgent.GetY());
-        SeedAgent mountainAgent2 = SeedAgent.Create(Mountain, snowAgent.GetX(), snowAgent.GetY() + 2);
-        SeedAgent mountainAgent3 = SeedAgent.Create(Mountain, snowAgent.GetX() - 2, snowAgent.GetY());
-        SeedAgent mountainAgent4 = SeedAgent.Create(Mountain, snowAgent.GetX(), snowAgent.GetY() - 2);
+        SeedAgent mountainAgent1 = SeedAgent.Create(Mountain, snowAgent.GetX() + SeedSpacing, snowAgent.GetY());
+        SeedAgent mountainAgent2 = SeedAgent.Create(Mountain, snowAgent.GetX(), snowAgent.GetY() + SeedSpacing);
+        SeedAgent mountainAgent3 = SeedAgent.Create(Mountain, snowAgent.GetX() - SeedSpacing, snowAgent.GetY());
+        SeedAgent mountainAgent4 = SeedAgent.Create(Mountain, snowAgent.GetX(), snowAgent.GetY() - SeedSpacing);
 
         // Verify mountain agents, then generate/verify grass agents
         // TODO add sand and water agents
         if (mountainAgent1 != null)
         {
             AgentQueue.Enqueue(mountainAgent1);
-            SeedAgent grassAgent = SeedAgent.Create(Grass, mountainAgent1.GetX(), mountainAgent1.GetY() + 2);
+            SeedAgent grassAgent = SeedAgent.Create(Grass, mountainAgent1.GetX(), mountainAgent1.GetY() + SeedSpacing);
             if (grassAgent != null)
                 AgentQueue.Enqueue(grassAgent);
         }
         if (mountainAgent2 != null)
         {
             AgentQueue.Enqueue(mountainAgent2);
-            SeedAgent grassAgent = SeedAgent.Create(Grass, mountainAgent1.GetX() + 2, mountainAgent1.GetY());
+            SeedAgent grassAgent = SeedAgent.Create(Grass, mountainAgent1.GetX() + SeedSpacing, mountainAgent1.GetY());
             if (grassAgent != null)
                 AgentQueue.Enqueue(grassAgent);
         }
         if (mountainAgent3 != null)
         {
             AgentQueue.Enqueue(mountainAgent3);
-            SeedAgent grassAgent = SeedAgent.Create(Grass, mountainAgent1.GetX(), mountainAgent1.GetY() - 2);
+            SeedAgent grassAgent = SeedAgent.Create(Grass, mountainAgent1.GetX(), mountainAgent1.GetY() - SeedSpacing);
             if (grassAgent != null)
                 AgentQueue.Enqueue(grassAgent);
         }
         if (mountainAgent4 != null)
         {
             AgentQueue.Enqueue(mountainAgent4);
-            SeedAgent grassAgent = SeedAgent.Create(Grass, mountainAgent1.GetX() - 2, mountainAgent1.GetY());
+            SeedAgent grassAgent = SeedAgent.Create(Grass, mountainAgent1.GetX() - SeedSpacing, mountainAgent1.GetY());
             if (grassAgent != null)
                 AgentQueue.Enqueue(grassAgent);
         }
