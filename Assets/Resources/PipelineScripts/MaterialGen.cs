a62d4d97a7e6c659799bd179ba70a9060b19ebf1
juanbecerra0 <47235983+juanbecerra0@users.noreply.github.com>
2020-05-07 14:57:45 -0600

    Implemented texture sampling

--- Assets/Resources/PipelineScripts/MaterialGen.cs
+++ Assets/Resources/PipelineScripts/MaterialGen.cs
@@ -23,6 +23,12 @@ public class MaterialGen : MonoBehaviour
     private Color MountainColor;
     private Color SnowColor;
 
+    // Textures
+    private Texture2D SandTexture;
+    private Texture2D GrassTexture;
+    private Texture2D MountainTexture;
+    private Texture2D LavaTexture;
+
     public void InitTexture(int textureResolution, int heightmapBaseN, Tuple<uint, uint, uint, uint, uint> BiomeTuple, Tuple<Color, Color, Color, Color, Color> ColorTuple)
     {
         this.textureResolution = textureResolution;
@@ -40,6 +46,31 @@ public class MaterialGen : MonoBehaviour
         GrassColor = ColorTuple.Item3;
         MountainColor = ColorTuple.Item4;
         SnowColor = ColorTuple.Item5;
+
+        Texture2D duplicateTexture(Texture2D source)
+        {
+            RenderTexture renderTex = RenderTexture.GetTemporary(
+                        source.width,
+                        source.height,
+                        0,
+                        RenderTextureFormat.Default,
+                        RenderTextureReadWrite.Linear);
+
+            Graphics.Blit(source, renderTex);
+            RenderTexture previous = RenderTexture.active;
+            RenderTexture.active = renderTex;
+            Texture2D readableText = new Texture2D(source.width, source.height);
+            readableText.ReadPixels(new Rect(0, 0, renderTex.width, renderTex.height), 0, 0);
+            readableText.Apply();
+            RenderTexture.active = previous;
+            RenderTexture.ReleaseTemporary(renderTex);
+            return readableText;
+        }
+
+        SandTexture = duplicateTexture(Resources.Load("PipelinePrefabs/Textures/sand1") as Texture2D);
+        GrassTexture = duplicateTexture(Resources.Load("PipelinePrefabs/Textures/grass1") as Texture2D);
+        MountainTexture = duplicateTexture(Resources.Load("PipelinePrefabs/Textures/mountain1") as Texture2D);
+        LavaTexture = duplicateTexture(Resources.Load("PipelinePrefabs/Textures/lava1") as Texture2D);
     }
 
     public Texture2D GenerateTexture(float[,] heightmap, uint[,] biomeMap)
@@ -70,20 +101,20 @@ public class MaterialGen : MonoBehaviour
             );
         }
 
-        Color GetColor(Tuple<int, int> heightmapIndex)
+        Color GetColor(Tuple<int, int> heightmapIndex, int x, int z)
         {
             uint index = biomeMap[heightmapIndex.Item1, heightmapIndex.Item2];
 
             if (index == WaterIndex)
                 return WaterColor;
             else if (index == SandIndex)
-                return SandColor;
+                return SandTexture.GetPixel(x, z);
             else if (index == GrassIndex)
-                return GrassColor;
+                return GrassTexture.GetPixel(x, z);
             else if (index == MountainIndex)
-                return MountainColor;
+                return MountainTexture.GetPixel(x, z);
             else if (index == SnowIndex)
-                return SnowColor;
+                return LavaTexture.GetPixel(x, z);
             else
                 return Color.red;
         }
@@ -96,7 +127,7 @@ public class MaterialGen : MonoBehaviour
             {
                 // Get delta average of heightmap, then set corresponding color
                 Tuple<int, int> heightmapIndex = GetHeightmapIndex(new Tuple<int, int>(i, j));
-                texture.SetPixel(j, textureResolution - i, GetColor(heightmapIndex));
+                texture.SetPixel(j, textureResolution - i, GetColor(heightmapIndex, j, textureResolution - i));
             }
         }
 
